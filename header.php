6551cee36ecb74313ca50d188a82173983a7e8d2
srprak <91665016+srprak@users.noreply.github.com>
2021-10-02 10:13:39 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>Simplification Engineering Test</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <div class="header">
+	<h1>Simplification Engineering Test App</h1>
+    </div>
